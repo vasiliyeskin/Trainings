d1410b20b5805694a65164e73a424f014a659390
vasiliyeskin <eskin@rf.unn.ru>
2017-11-07 17:56:47 +0300

    Lesson 1 - select, distinct, comments.

---
+++ src/SQL_in_10_minutes/lesson2.sql
@@ -0,0 +1,13 @@
+SELECT prod_name FROM PRODUCTS;
+
+SELECT prod_id, prod_name, prod_price FROM PRODUCTS;
+
+SELECT * FROM products;
+
+SELECT vend_id FROM products;
+
+SELECT DISTINCT vend_id FROM products;
+
+SELECT prod_name FROM products LIMIT 5;
+
+SELECT prod_name FROM products LIMIT 5 OFFSET 5;
