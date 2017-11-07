deab8cd20c4efa905f82c297328b5e95797fd008
vasiliyeskin <eskin@rf.unn.ru>
2017-11-07 23:42:51 +0300

    Lesson 3 - order by, desc.

---
+++ src/SQL_in_10_minutes/lesson3.sql
@@ -0,0 +1,10 @@
+SELECT prod_name FROM products ORDER BY prod_name
+
+SELECT prod_id, prod_price, prod_name FROM products ORDER BY prod_price, prod_name;
+
+SELECT prod_id, prod_price, prod_name FROM products ORDER BY 2, 3;
+
+SELECT prod_id, prod_price, prod_name FROM products ORDER BY prod_price DESC ;
+
+SELECT prod_id, prod_price, prod_name FROM products ORDER BY prod_price DESC, prod_name;
+
