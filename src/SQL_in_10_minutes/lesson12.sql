39d90850100ceeaaaa9b84ab5c995a21d49a7c6f
vasiliyeskin <eskin@rf.unn.ru>
2017-11-24 17:36:26 +0300

    Lesson 12 - inner join

--- src/SQL_in_10_minutes/lesson12.sql
+++ src/SQL_in_10_minutes/lesson12.sql
@@ -15,8 +15,10 @@ WHERE vendors.vend_id = products.vend_id
 and orderitems.prod_id = products.prod_id
 and order_num = 20007;
 
+
 SELECT cust_name, cust_contact
 FROM customers, orders, orderitems
 WHERE customers.cust_id = orders.cust_id
 AND orderitems.order_num = orders.order_num
 and prod_id='RGAN01';
+
