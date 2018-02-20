175c5445ddfbc1422e592077d5156d036ed52dc1
vasiliyeskin <eskin@rf.unn.ru>
2018-02-20 19:16:15 +0300

    lesson 18 - CREATE VIEW

---
+++ src/SQL_in_10_minutes/lesson18.sql
@@ -0,0 +1,15 @@
+SELECT cust_name, cust_contact
+FROM customers, orders, orderitems
+WHERE customers.cust_id = orders.cust_id
+AND orderitems.order_num = orders.order_num
+and prod_id='RGAN01';
+
+CREATE VIEW ProductCustomers AS
+SELECT cust_name, cust_contact, prod_id
+FROM customers, orders, orderitems
+WHERE customers.cust_id = orders.cust_id
+AND orderitems.order_num = orders.order_num;
+
+SELECT cust_name, cust_contact
+FROM ProductCustomers
+WHERE prod_id='RGAN01';
